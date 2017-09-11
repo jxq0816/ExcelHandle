fdb3338909a2bbad82715af2873ee99bd6aa65ce
jiangxingqi <jxq0816@126.com>
2017-09-11 18:52:44 +0800

    数据处理

--- src/main/java/com/week7i/share/ReadExcel.java
+++ src/main/java/com/week7i/share/ReadExcel.java
@@ -41,27 +41,32 @@ public class ReadExcel {
                     titleRowList.add(cellString);
                 }
             }
-            System.out.println(titleRowList);
-            result.add(titleRowList);
+            //System.out.println(titleRowList);
+            //result.add(titleRowList);
 
             //处理当前页，循环读取每一行
-            for (int rowNum = 1; rowNum <= hssfSheet.getLastRowNum(); rowNum++) {
+            for (int rowNum = 2; rowNum <= hssfSheet.getLastRowNum(); rowNum++) {
                 XSSFRow hssfRow = hssfSheet.getRow(rowNum);
-                int minColIndex = hssfRow.getFirstCellNum();
                 int maxColIndex = hssfRow.getLastCellNum();
-                List<String> rowList = new ArrayList<String>();
+                XSSFCell idCell = hssfRow.getCell(0);
+                String id=idCell.toString();
                 //遍历该行，获取处理每个cell元素
-                for (int colIndex = minColIndex; colIndex < maxColIndex; colIndex++) {
-                    if ((colIndex == 0) || (colIndex % 12 == num)) {
+                for (int colIndex = 1; colIndex < maxColIndex; colIndex++) {
+                    if ((num==12&&colIndex%12==0) || colIndex % 12 == num) {
+                        int titleIndex=(colIndex-1)/12+1;
+                        String title=titleRowList.get(titleIndex);
+                        List<String> rowList = new ArrayList<String>();
                         XSSFCell cell = hssfRow.getCell(colIndex);
                         if (cell == null) {
                             continue;
                         }
-                        //System.out.println(cell.toString());
+                        rowList.add(id);
+                        rowList.add(title);
                         rowList.add(cell.toString());
+                        result.add(rowList);
                     }
                 }
-                result.add(rowList);
+
             }
         }
         return result;
@@ -70,7 +75,7 @@ public class ReadExcel {
     public static void main(String[] args) throws IOException {
         //String path="Users/jiangxingqi/IdeaProjects/ExcelUtil/doc/huizong.xlsx";
         String path = "doc/huizong.xlsx";
-        for(int num=6;num<=12;num++){
+        for(int num=12;num<=12;num++){
             List<List<String>> result = read(path,num);
             for (int i = 0; i < result.size(); i++) {
                 System.out.println(result.get(i));
