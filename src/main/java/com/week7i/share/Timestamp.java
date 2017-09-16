263c2351ede40205d8dd1b480f72c497611d6ec3
jiangxingqi <jxq0816@126.com>
2017-09-17 02:49:35 +0800

    数据处理

--- src/main/java/com/week7i/share/Timestamp.java
+++ src/main/java/com/week7i/share/Timestamp.java
@@ -21,7 +21,7 @@ import java.util.List;
 
 public class Timestamp {
 
-    public static List read(String path) throws IOException, ParseException {
+    public static List arrive(String path) throws IOException, ParseException {
         List rs=new ArrayList();
         InputStream inputStream = new FileInputStream(path);
         XSSFWorkbook hssfWorkbook = new XSSFWorkbook(inputStream);
@@ -48,7 +48,40 @@ public class Timestamp {
                 XSSFCell aircraft = hssfRow.getCell(6);
                 String aircraftId = aircraft.toString();
                 rs.add(aircraftId);
-                System.out.println(aircraftId);
+                //System.out.println(aircraftId);
+            }
+        }
+        return rs;
+    }
+
+    public static List leave(String path) throws IOException, ParseException {
+        List rs=new ArrayList();
+        InputStream inputStream = new FileInputStream(path);
+        XSSFWorkbook hssfWorkbook = new XSSFWorkbook(inputStream);
+        List<List<String>> result = new ArrayList<List<String>>();
+        XSSFSheet xssfSheet = hssfWorkbook.getSheetAt(0);
+        //处理当前页，循环读取每一行
+        for (int rowNum = 1; rowNum <= xssfSheet.getLastRowNum(); rowNum++) {
+            XSSFRow hssfRow = xssfSheet.getRow(rowNum);
+            boolean b1 = false;//从OVS出发
+            XSSFCell start = hssfRow.getCell(3);
+            String cellData = start.toString();
+            if (cellData.equals("OVS")) {
+                b1 = true;
+            }
+            boolean b2 = false;//于21:00之后出发
+            XSSFCell startTime = hssfRow.getCell(1);
+            String startTimeString = startTime.toString();
+            BigDecimal bd = new BigDecimal(startTimeString);
+            Long l = Long.parseLong(bd.toPlainString());
+            if (l > 1461358800) {
+                b2 = true;
+            }
+            if (b1 && b2) {
+                XSSFCell aircraft = hssfRow.getCell(6);
+                String aircraftId = aircraft.toString();
+                rs.add(aircraftId);
+                //System.out.println(aircraftId);
             }
         }
         return rs;
@@ -57,7 +90,19 @@ public class Timestamp {
     public static void main(String[] args) throws IOException, ParseException {
         //String path="Users/jiangxingqi/IdeaProjects/ExcelUtil/doc/huizong.xlsx";
         String path = "doc/calculate.xlsx";
-        List rs=read(path);
+        List rs=new ArrayList();
+        List arriveList=arrive(path);
+        List leaveList=leave(path);
+        for(int i=0;i<arriveList.size();i++){
+            String arriveId=arriveList.get(i).toString();
+            for(int j=0;j<leaveList.size();j++){
+                String leaveId=leaveList.get(j).toString();
+                if(arriveId.equals(leaveId)){
+                    rs.add(leaveId);
+                    break;
+                }
+            }
+        }
         System.out.println(rs.size());
     }
 }
