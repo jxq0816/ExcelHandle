46560afdb1cc2e917824d8c92c0da6014d6a75bd
jiangxingqi <jxq0816@126.com>
2017-09-19 02:16:04 +0800

    Q2已解

--- src/main/java/com/week7i/share/Question2.java
+++ src/main/java/com/week7i/share/Question2.java
@@ -110,10 +110,10 @@ public class Question2 {
                 diffA=(Calculate.timestamp2100-startTimeStamp);
             }
             stayTimeSum+=diffA;//A航班的延迟 计入 总延时
-            if(startTime<Calculate.timestamp2145){//B航班有延迟
+            /*if(startTime<Calculate.timestamp2145){//B航班有延迟
                 long diff=Calculate.timestamp2145-startTimeStamp;//B航班的延迟
                 stayTimeSum+=diff;
-            }
+            }*/
             //System.out.println("航班："+schedule);
             //System.out.println("延时"+stayTimeSum/60+"分钟");
             if(i==0){
@@ -125,85 +125,27 @@ public class Question2 {
         }
         JSONObject replaceSchedule=(JSONObject)availableList.get(index);//选择延时最小的航班作为替换
         String aircraftId=replaceSchedule.getString("aircraftId");
-        System.out.print("飞机尾号"+aircraftId+",");
-        System.out.println("两架飞机延误时间总和为:"+min/60+"分钟");
+        String rowNum=replaceSchedule.getString("rowNum");
+        System.out.print("第"+rowNum+"行，飞机尾号: "+aircraftId+",");
+        System.out.println("延迟为:"+min/60+"分钟");
 
         return replaceSchedule;
     }
     public static void main(String[] args) throws IOException, ParseException {
         //availableListShow();
-        List availableList=Calculate.available(path,lastRowNum);
-        List saveList=Calculate.saveList(path,lastRowNum);//获得可供替换的航班集合,共计7个
-        for(int i=0;i< saveList.size();i++){
-            JSONObject object= (JSONObject) saveList.get(i);
-            String rowNum=object.getString("rowNum");
-            String aircraftId=object.getString("aircraftId");
-            Long scheduleIdLong=object.getLong("scheduleIdLong");
-            Long startTimeLong=object.getLong("startTimeLong");
-            String aircraftType=object.getString("aircraftType");
-            System.out.print("行号："+rowNum+"，航班："+scheduleIdLong+"，飞机尾号"+aircraftId+"置换");
-            JSONObject index=judge(availableList,startTimeLong,aircraftType);
+        List availableList = Calculate.available(path, lastRowNum);
+        List saveList = Calculate.saveList(path, lastRowNum);//获得可供替换的航班集合,共计7个
+        for (int i = 0; i < saveList.size(); i++) {
+            JSONObject object = (JSONObject) saveList.get(i);
+            String rowNum = object.getString("rowNum");
+            String aircraftId = object.getString("aircraftId");
+            Long scheduleIdLong = object.getLong("scheduleIdLong");
+            Long startTimeLong = object.getLong("startTimeLong");
+            String aircraftType = object.getString("aircraftType");
+            System.out.print("行号：" + rowNum + "，航班：" + scheduleIdLong + "，飞机尾号：" + aircraftId + " 置换 ");
+            JSONObject index = judge(availableList, startTimeLong, aircraftType);
+            availableList.remove(index);
         }
-/*
-        //处理35行数据，即174773460次航班,起飞时间戳为1461358200，飞机尾号14098
-        System.out.print("174773460航班，飞机尾号14098置换");
-        JSONObject index=judge(availableList,1461358200,"9");
-        availableList.remove(index);
-
-        //处理42行数据，即174774204次航班,起飞时间戳为1461359100,飞机尾号44098
-        System.out.print("174774204航班，飞机尾号44098置换");
-        index=judge(availableList,1461359100L,"9");
-        availableList.remove(index);
-
-        //处理51行数据，即174773432次航班,起飞时间戳为1461358200,飞机尾号64098
-        System.out.print("174773432航班，飞机尾号64098置换");
-        index=judge(availableList,1461358200L,"9");
-        availableList.remove(index);
-
-        //处理59行数据，即174774076次航班,起飞时间戳为1461355500,飞机尾号15098
-        System.out.print("174774076航班，飞机尾号15098置换");
-        index=judge(availableList,1461355500L,"9");
-        availableList.remove(index);
-
-        //处理69行数据，即174774048次航班,起飞时间戳为1461354300,飞机尾号85098
-        System.out.print("174774048航班，飞机尾号85098置换");
-        index=judge(availableList,1461354300L,"9");
-        availableList.remove(index);
-
-        //处理156行数据
-        System.out.print("174773877航班，飞机尾号CKBPV置换");
-        index=judge(availableList,1461353100L,"320");
-        availableList.remove(index);
-
-        //处理178行数据
-        System.out.print("174774048航班，飞机尾号LLBPV置换");
-        index=judge(availableList,1461353100,"32A");
-        availableList.remove(index);
-
-        //处理309行数据
-        System.out.print("174773988航班，飞机尾号GTBPV置换");
-        index=judge(availableList,1461358200,"321");
-        availableList.remove(index);
-
-        //处理340行数据
-        System.out.print("174774178航班，飞机尾号DWBPV置换");
-        index=judge(availableList,1461353400,"320");
-        availableList.remove(index);
-
-        //处理384行数据
-        System.out.print("174773791航班，飞机尾号QZBPV置换");
-        index=judge(availableList,1461354000,"320");
-        availableList.remove(index);
-
-        //处理390行数据
-        System.out.print("174773382航班，飞机尾号RZBPV置换");
-        index=judge(availableList,1461353400,"320");
-        availableList.remove(index);
-
-        //处理439行数据
-        System.out.print("174773382航班，飞机尾号RZBPV置换");
-        index=judge(availableList,1461353400,"320");
-        availableList.remove(index);*/
     }
 }
 
